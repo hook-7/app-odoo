3fd696717d5fae0b3399ae6829bdd4e885cb18b4
Ivan Office <guohuadeng@hotmail.com>
2024-11-12 12:44:52 +0800

    update desc

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'odoo Tweak,Ai Employee,Boost,Customize All in One. Customize,UI,Boost,Security,Data',
-    'version': '16.24.11.08',
+    'version': '16.24.11.12',
     'author': 'odooai.cn',
     'category': 'Extra Tools',
     'website': 'https://www.odooai.cn',
@@ -147,7 +147,8 @@
     45. Add addons path info to module.
     46. Add Help documentation anywhere.  easy get help for any odoo operation or action.
     47. Add ai robot app integration. Use Ai as your employee.
-    48. Add menu debug information of action and xml id.
+    48. Add configurable Test Label, Demo Ribbon on top menu.
+    49. Add SaaS client switch for online Translate sync(18 only).
 
     This module can help to white label the Odoo.
     Also helpful for training and support for your odoo end-user.
@@ -203,6 +204,7 @@
     45. 为应用模块增加模块路径信息
     46. 增加快速帮助文档，可以在任意操作中获取相关的 odoo 帮助.
     47. 增加Ai模块相关信息，可以快速访问ai模块，使用ai员工.
-    48. 增加菜单Debug，可以快速查看每个菜单的调试信息如 action 及 xml id.
+    48. 增加可配置的系统调试标签，用于系统测试期提示.
+    49. 增加 SaaS 客户端开头，可让用户安装在线翻译同步模块及在线更新(仅odoo18).
     """,
 }
